8e90949b578f18c44d0ba2f148e50ebf601ea34a
Jordigt11 <jordi.gomez@estudiant.enti.cat>
2026-01-04 13:42:52 +0100

    Interfaz del programa
    La base de la interfaz del programa ya esta hecha, hay que añadir la funcion de los botones de la clase swing, y la clase authController es la que cambia la ventana y hace las funciones de los botones

--- Projectos/Autentication/src/app/App.java
+++ Projectos/Autentication/src/app/App.java
@@ -1,41 +1,20 @@
 package app;
 
+import controller.AuthContoller;
 import model.EstadoSesion;
+import view.Swing;
 
 public class App {  
     //MAIN
     public static void main(String[] args) {
-        App app = new App();
-        app.iniciar();
-    }
-
-    public void iniciar(){
         EstadoSesion estado = EstadoSesion.Esperando_Opcion;
-        boolean continuar = true;
-        new view.Swing();
+        AuthContoller controller = new AuthContoller();
+        Swing ventana = new Swing(controller);
+        ventana.mostrarVentana(estado);
         
-        do{
-            switch (estado) {
-                case Esperando_Opcion:
-                    
-                    break;
-                case Procesando_MFA:
-                    
-                    break;
-                case Procesando_Login:
-
-                    break;
-                case Procesando_Autenticacion:
-
-                    break;
-                case Sesion_Activa:
-
-                    break;
-                case Bloqueado:
-                    return;
-            }
-        }while(continuar);
     }
+
+    
 }
 
 
--- Projectos/Autentication/src/view/Swing.java
+++ Projectos/Autentication/src/view/Swing.java
@@ -1,18 +1,115 @@
 package view;
 import javax.swing.*;
+import java.awt.*;
+
+import controller.AuthContoller;
+import model.EstadoSesion;
 
 public class Swing {
     
-    public Swing(){
+    private JFrame frame;
+    private JPanel panel;
+    private AuthContoller controller;
 
-        JFrame frame = new JFrame();
-        frame.setTitle("Authenticator Multifactor");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(900,600);
-        frame.setVisible(true);
-        frame.setLocationRelativeTo(null);
+    public Swing(AuthContoller controller){
+
+        this.controller = controller;
         
+        frame = new JFrame("Authenticator MFA"); //Nombre de la ventana
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Al cerrar la aplicacion se termina el programa
+        frame.setSize(900, 600);
 
+        panel = new JPanel();
+        panel.setLayout(new GridLayout(0,1)); //Para alinear los componentes de la vista, botones, campos de texto etc
+        frame.add(panel);
+
+        frame.setLocationRelativeTo(null); //La app se abre en el centro de la pantalla
+        frame.setVisible(true); //Para que se muestre la ventana
+    }
+
+    public void mostrarVentana(EstadoSesion estado){
+        panel.removeAll(); // Se hace para que al cambiar de estado se muestren otros componentes
+
+        switch (estado) {
+            case Esperando_Opcion:
+                menuEsperandoOpcion();
+                break;
+            case Procesando_MFA:
+                menuProcesandoMFA();
+                break;
+            case Procesando_Login:
+                menuProcesandoLogin();
+                break;
+            case Procesando_Autenticacion:
+                menuProcesandoAutenticacion();
+                break;
+            case Sesion_Activa:
+                menuSesionActiva();
+                break;
+            case Bloqueado:
+                menuBloqueado();
+                break;
+        }
+
+        panel.revalidate();
+        panel.repaint();
+    }
+
+    private void menuEsperandoOpcion(){
+        //Para el estado Esperando_Opcion ponemos 2 botones
+        JButton mfa = new JButton("Añadir cuenta MFA");
+        JButton iniciarSesion = new JButton("Iniciar Sesion");
+        /*hay que añadir las acciones de los botones, la clase authcontroller
+        es la que debe de hacer las acciones correspondientes de los botones*/
+
+
+        panel.add(mfa);
+        panel.add(iniciarSesion);
+
+    }
+
+    private void menuProcesandoMFA(){
+        JTextField correo = new JTextField();
+        JButton boton = new JButton("Añadir");
+        /*añadir la accion del boton*/
+
+
+        panel.add(new JLabel("Correo electronico:"));
+        panel.add(correo);
+        panel.add(boton);
+    }
+
+    private void menuProcesandoLogin(){
+        JTextField usuario = new JTextField();
+        JTextField contraseña = new JPasswordField();
+        JButton boton = new JButton("Confirmar");
+        /*añadir la accion del boton */
+
+
+        panel.add(new JLabel("Usuario:"));
+        panel.add(usuario);
+        panel.add(new JLabel("Contraseña:"));
+        panel.add(contraseña);
+        panel.add(boton);
+    }
+
+    private void menuProcesandoAutenticacion(){
+        JTextField codigoTOTP = new JTextField();
+        JButton boton = new JButton("Confirmar");
+        /*añadir la accion del boton */
+
+
+        panel.add(new JLabel("Introducir codigo MFA:"));
+        panel.add(codigoTOTP);
+        panel.add(boton);
+    }
+
+    private void menuSesionActiva(){
+        panel.add(new JLabel("Has iniciado sesion"));
+    }
+
+    private void menuBloqueado(){
+        panel.add(new JLabel("Se ha bloqueado la cuenta"));
     }
 
 }
